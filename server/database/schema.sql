db1dafd721cc2f8c191a28d351ef1e52f0959a97
pranjalidixitnavriti <pranjalidixit.navriti@gmail.com>
2026-09-02 17:50:12 +0530

    add users table schema to database/schema.sql

---
+++ server/database/schema.sql
@@ -0,0 +1,15 @@
+--Admin Management System - Database Schema
+--Reference file
+--Sprint 1- Foundation tables
+
+--users table
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    is_active BOOLEAN DEFAULT TRUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
