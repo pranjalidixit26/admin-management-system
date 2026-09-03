761289d5f3a0fd0ccbd2f6636d4c10ae5fccd0ca
pranjalidixitnavriti <pranjalidixit.navriti@gmail.com>
2026-09-03 10:59:54 +0530

    Create User TypeOrm entity

---
+++ server/src/users/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column({ default: true })
+    is_active: boolean;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
