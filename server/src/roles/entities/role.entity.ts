76ee64776c9d431c8b057fe3efec9f90fadb03a7
pranjalidixitnavriti <pranjalidixit.navriti@gmail.com>
2026-09-03 11:30:46 +0530

    Create Role TypeOrm entity

---
+++ server/src/roles/entities/role.entity.ts
@@ -0,0 +1,36 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    ManyToMany,
+    JoinTable
+} from 'typeorm';
+import {User} from '../../users/entities/user.entity';
+
+@Entity('roles')
+export class Role {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    name: string;
+
+    @Column({ nullable: true })
+    description: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @ManyToMany(() => User)
+    @JoinTable({
+        name: 'user_roles',
+        joinColumn: {name: 'role_id', referencedColumnName: 'id'},
+        inverseJoinColumn: {name: 'user_id', referencedColumnName: 'id'},
+    })
+    users: User[];
+}
