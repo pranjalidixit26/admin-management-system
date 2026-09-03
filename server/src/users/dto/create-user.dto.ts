23cc0f989ba6fffa24ec7cabfd31d7514d6962ca
pranjalidixitnavriti <pranjalidixit.navriti@gmail.com>
2026-09-03 18:26:02 +0530

    Implement Users CRUD (service, controller, DTOs)

---
+++ server/src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, IsEmail, MinLength, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    name: string;
+
+    @IsEmail()
+    email: string;
+
+    @MinLength(6)
+    password: string;
+
+    @IsOptional()
+    @IsBoolean()
+    status?: boolean;
+}
