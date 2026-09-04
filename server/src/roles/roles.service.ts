316a45ce9f8e2271c79cb1960c60127f2da8e142
pranjalidixitnavriti <pranjalidixit.navriti@gmail.com>
2026-09-04 12:10:03 +0530

    Implement Roles CRUD (service, controller, DTOs)

---
+++ server/src/roles/roles.service.ts
@@ -0,0 +1,42 @@
+import { Injectable , NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from 'typeorm';
+import { Role } from './entities/role.entity';
+import { CreateRoleDto } from "./dto/create-role.dto";
+import { UpdateRoleDto } from "./dto/update-role.dto";
+
+@Injectable()
+export class RolesService {
+    constructor(
+        @InjectRepository(Role)
+        private readonly roleRepository: Repository<Role>,
+    ) {}
+
+    async create(createRoleDto: CreateRoleDto): Promise<Role> {
+        const role =  this.roleRepository.create(createRoleDto);
+        return this.roleRepository.save(role);
+    }
+
+    async findAll(): Promise<Role[]> {
+        return this.roleRepository.find();
+    }
+
+    async findOne(id: number): Promise<Role> {
+        const role = await this.roleRepository.findOne({ where: { id }});
+        if(!role) {
+            throw new NotFoundException(`Role with id ${id} not found`);
+        }
+        return role;
+    }
+
+    async update(id: number, updateRoleDto: UpdateRoleDto): Promise<Role> {
+        const role = await this.findOne(id);
+        Object.assign(role, updateRoleDto);
+        return this.roleRepository.save(role);
+    }
+
+    async remove(id: number): Promise<void> {
+        const role = await this.findOne(id);
+        await this.roleRepository.remove(role);
+    }
+}
